fe8dd1764a83d7f221ca45876928ec42666f930b
rosellij <78378176+rosellij@users.noreply.github.com>
2021-03-21 11:41:05 -0400

    filetime

---
+++ kittystartup.py
@@ -0,0 +1,3 @@
+import Adafruit_CharLCD as LCD
+(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_backlight, lcd_columns, lcd_rows) = (25, 24, 23, 17, 18, 22, 4, 16, 2)
+lcd = LCD.Adafruit_CharLCD(25, 24, 23, 17, 18, 22, 16, 2, 4)
