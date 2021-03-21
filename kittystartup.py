2684973d06a2f46a6b250dda00cb6cf5e220f259
rosellij <78378176+rosellij@users.noreply.github.com>
2021-03-21 11:57:53 -0400

    push!

--- kittystartup.py
+++ kittystartup.py
@@ -1,3 +1,6 @@
-import Adafruit_CharLCD as LCD
-(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_backlight, lcd_columns, lcd_rows) = (25, 24, 23, 17, 18, 22, 4, 16, 2)
-lcd = LCD.Adafruit_CharLCD(25, 24, 23, 17, 18, 22, 16, 2, 4)
+def kittyfunction():
+    
+    import Adafruit_CharLCD as LCD
+    (lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_backlight, lcd_columns, lcd_rows) = (25, 24, 23, 17, 18, 22, 4, 16, 2)
+    lcd = LCD.Adafruit_CharLCD(25, 24, 23, 17, 18, 22, 16, 2, 4)
+    return "You did the thing!"
